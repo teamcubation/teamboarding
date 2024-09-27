114c9dc08202a6540e02185e31a1969e404f181b
Jonathan Tapicer <tapicer@gmail.com>
2024-09-27 11:54:49 -0300

    Responsive versions.

--- convert.py
+++ convert.py
@@ -30,17 +30,28 @@ add_to_head("""<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">""")
 add_to_head('<meta name="description" content="Simplify and enhance your onboarding experience, ensuring every new team member is set up for success from day one.">')
-replace_text('<div class="frame-58">', '<div class="frame-58" style="width: 545px;">')
+replace_text('<body>', '<body style="overflow-x: hidden;">')
+replace_text('<div class="frame-184">', '<div class="frame-184" style="width: 545px;">')
 replace_text('<input class="input-2" placeholder="Fullname" type="text" />', '<input class="input-2" placeholder="Fullname" name="fullname" style="color: #000;" type="text" />')
-replace_text('<div class="input-4"><div class="text-wrapper-58">Company</div></div>', '<input class="input-2" placeholder="Company" name="company" style="color: #000;" type="text">')
+replace_text('<div class="input-4"><div class="text-wrapper-57">Company</div></div>', '<input class="input-2" placeholder="Company" name="company" style="color: #000;" type="text">')
 replace_text('<input class="input-5" placeholder="Company Email" type="email" />', '<input class="input-5" placeholder="Company Email" name="email" style="color: #000;" type="email" />')
+replace_text('<div class="link-3">', '<div class="link-3" style="padding: 12px 24px; cursor: pointer;">')
 replace_text('<div class="link-2">', '<div class="link-2" style="padding: 12px 24px; cursor: pointer;">')
-replace_text('<img class="laptop-table-looks-2" src="img/laptop-table-looks-pretty-darkness-1-1.png" />', '<img class="laptop-table-looks-2" src="img/laptop-table-looks-pretty-darkness-1-1.png" style="width: 100%;" />')
+replace_text('<img class="laptop-table-looks-6" src="img/laptop-table-looks-pretty-darkness-1-5.png" />', '<img class="laptop-table-looks-6" src="img/laptop-table-looks-pretty-darkness-1-5.png" style="width: 100%;" />')
+replace_text('<img class="laptop-table-looks-4" src="img/laptop-table-looks-pretty-darkness-1-3.png" />', '<img class="laptop-table-looks-4" src="img/laptop-table-looks-pretty-darkness-1-3.png" style="width: 100%;" />')
+replace_text('<img class="laptop-table-looks-2" src="img/laptop-table-looks-pretty-darkness-1-1.png" />', '<img class="laptop-table-looks-2" src="img/laptop-table-looks-pretty-darkness-1-1.png" style="width: 100%; height: 100%;" />')
 replace_text('<div class="title-with-emoji">', '<div class="title-with-emoji" style="width: auto;">')
+replace_text('<div class="title-with-emoji-2">', '<div class="title-with-emoji-2" style="width: auto;">')
+replace_text('<div class="title-with-emoji-3">', '<div class="title-with-emoji-3" style="width: auto;">')
 replace_text('<span class="text-wrapper-4">&nbsp;</span>', '')
-replace_text('<img class="img" src="img/frame-276-2.png" />', '')
-replace_text('<div class="chips-4">', '<div class="chips-4" style="padding: 3px 6.25px;">')
-replace_text('<div class="chips-5">', '<div class="chips-5" style="padding: 3px 6.25px;">')
+replace_text('<img class="img-5" src="img/frame-276-20.png" />', '')
+replace_text('<img class="img-5" src="img/frame-276-11.png" />', '')
+replace_text('<img class="img-5" src="img/frame-276-10.png" />', '')
+replace_text('<div class="chips-17">', '<div class="chips-17" style="padding: 5px 6.25px 2px;">')
+replace_text('<div class="chips-18">', '<div class="chips-18" style="padding: 5px 6.25px 3px;">')
+replace_text('<div class="container-wrapper-3">', '<div class="container-wrapper-3" style="width: 300px;">')
+replace_text('<div class="container-wrapper-2">', '<div class="container-wrapper-2" style="width: 190px;">')
+replace_text('<div class="container-wrapper">', '<div class="container-wrapper" style="width: 90px;">')
 
 # add floating buttons
 add_to_head('<link rel="stylesheet" href="floating-buttons-style.css" />')
@@ -54,36 +65,44 @@ add_to_body("""
         </div>
       </div>
     </div>
-    <script type="text/javascript">
-      const floatingButtons = document.querySelector('.floating-buttons');
-      window.onscroll = function() {
-        if (window.pageYOffset > 600) {
-          floatingButtons.style.display = 'inline-flex';
-        } else {
-          floatingButtons.style.display = 'none';
-        }
-      };
-    </script>
 """)
 
 # add favicon
 add_to_head("""<link rel="icon" href="static/favicon.ico">""")
 
 # links
-replace_text_regex(r'<div class="header-header-nav">(.*?)</div>', '<a class="header-header-nav" href="#\\1">\\1</a>')
 replace_text_regex(r'<div class="header-header-nav-2">(.*?)</div>', '<a class="header-header-nav-2" href="#\\1">\\1</a>')
 replace_text_regex(r'<div class="header-header-nav-3">(.*?)</div>', '<a class="header-header-nav-3" href="#\\1">\\1</a>')
 replace_text_regex(r'<div class="header-header-nav-4">(.*?)</div>', '<a class="header-header-nav-4" href="#\\1">\\1</a>')
-replace_text('<div class="frame-8">', '<div class="frame-8"><a name="About"></a>')
-replace_text('<div class="section">', '<div class="section"><a name="Customers"></a>')
-replace_text('<div class="frame-19">', '<a name="Features"></a><div class="frame-19">')
-replace_text('<div class="overlap-group-3">', '<div class="overlap-group-3"><a name="Integrations"></a>')
-replace_text('<div class="frame-67">', '<div class="frame-67"><a name="Benefits"></a>')
-replace_text('<div class="text-wrapper">Sign in</div>', '<a style="line-height: 10px;" class="text-wrapper" href="https://app.teamcubation.com/login" target="_blank">Sign in</a>')
-replace_text('<div class="text-wrapper">Talk to Sales</div>', '<a style="line-height: 10px;" class="text-wrapper" href="https://calendly.com/natalia-perez-tq/teamboarding-talk-to-sales" target="_blank">Talk to Sales</a>')
+replace_text_regex(r'<div class="header-header-nav-5">(.*?)</div>', '<a class="header-header-nav-5" href="#\\1">\\1</a>')
+replace_text_regex(r'<div class="header-header-nav-6">(.*?)</div>', '<a class="header-header-nav-6" href="#\\1">\\1</a>')
+replace_text('<div class="frame-132">', '<div class="frame-132"><a name="About"></a>')
+replace_text('<div class="frame-78">', '<div class="frame-78"><a name="About"></a>')
+replace_text('<div class="div-rounded-xl-wrapper">', '<a name="About"></a><div class="div-rounded-xl-wrapper">')
+replace_text('<div class="section-7">', '<div class="section-7"><a name="Customers"></a>')
+replace_text('<div class="section-4">', '<div class="section-4"><a name="Customers"></a>')
+replace_text('<div class="section">', '<div class="section-4"><a name="Customers"></a>')
+replace_text('<div class="frame-145">', '<a name="Features"></a><div class="frame-145">')
+replace_text('<div class="frame-89">', '<a name="Features"></a><div class="frame-89">')
+replace_text('<div class="frame-17">', '<a name="Features"></a><div class="frame-17">')
+replace_text('<div class="overlap-group-9">', '<div class="overlap-group-9"><a name="Integrations"></a>')
+replace_text('<div class="frame-56">', '<div class="frame-56"><a name="Integrations"></a>')
+replace_text('<div class="frame-194">', '<div class="frame-194"><a name="Benefits"></a>')
+replace_text('<div class="frame-125">', '<div class="frame-125"><a name="Benefits"></a>')
+replace_text('<div class="frame-63">', '<div class="frame-63"><a name="Benefits"></a>')
+replace_text('<div class="header-header-nav"><div class="text-wrapper">Sign in</div></div>', '<a class="header-header-nav" href="https://app.teamcubation.com/login" target="_blank"><div style="line-height: 10px;" class="text-wrapper">Sign in</div></a>')
+replace_text('<div class="header-header-nav"><div class="text-wrapper">Talk to Sales</div></div>', '<a class="header-header-nav" href="https://calendly.com/natalia-perez-tq/teamboarding-talk-to-sales" target="_blank"><div style="line-height: 10px;" class="text-wrapper">Talk to Sales</div></a>')
+replace_text('<div class="section-5"', '<a name="Start"></a><div class="section-5"')
+replace_text('<div class="section-8"', '<a name="Start"></a><div class="section-8"')
 replace_text('<div class="section-2"', '<a name="Start"></a><div class="section-2"')
 replace_text('<div class="link"><div class="text-wrapper-3">Start for free</div></div>', '<a class="link" href="#Start"><div class="text-wrapper-3">Start for free</div></a>')
 replace_text('<span class="text-wrapper-5">Get a demo</span>', '<a class="text-wrapper-5" style="cursor: pointer;" onclick="showDemoDialog()">Get a demo</a>')
+replace_text("""<p class="privacy-policy-terms-2">
+              Privacy Policy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Terms &amp; Conditions
+            </p>""",
+             """<p class="privacy-policy-terms-2">
+              <a href="https://teamcubation.com/privacy-policy.html" target="_blank" style="color: var(--tokens-text-secondary);">Privacy Policy</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://teamcubation.com/terms.html" target="_blank" style="color: var(--tokens-text-secondary);">Terms &amp; Conditions</a>
+            </p>""")
 replace_text("""<p class="privacy-policy-terms">
               Privacy Policy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Terms &amp; Conditions
             </p>""",
--- static/scripts.js
+++ static/scripts.js
@@ -13,3 +13,28 @@ function demoDialogOk() {
 function hideDemoDialog() {
     document.querySelector('.demo-dialog-container').style.display = 'none';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const floatingButtons = document.querySelector('.floating-buttons');
+    window.onscroll = function() {
+        if (window.pageYOffset > 600 && (document.body.scrollWidth >= 1440 || window.pageYOffset + document.body.clientHeight < document.body.scrollHeight - 50)) {
+            floatingButtons.style.display = 'inline-flex';
+        } else {
+            floatingButtons.style.display = 'none';
+        }
+    };
+
+    for (const section of ['Start', 'About', 'Customers', 'Features', 'Integrations', 'Benefits']) {
+        const allSectionButtons = document.querySelectorAll(`[href="#${section}"]`)
+        for (let i = 0; i < allSectionButtons.length; i++) {
+            allSectionButtons[i].addEventListener('click', function() {
+                const sectionAnchors = document.querySelectorAll(`a[name="${section}"]`);
+                for (let i = 0; i < sectionAnchors.length; i++) {
+                    if (sectionAnchors[i].offsetParent) {
+                        sectionAnchors[i].scrollIntoView();
+                    }
+                }
+            });
+        }
+    }
+});
