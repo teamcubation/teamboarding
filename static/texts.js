8a5492ab6b442733ec5c597a29ea7357a3c6d2ab
Jonathan Tapicer <tapicer@gmail.com>
2024-12-05 11:47:48 -0300

    Different versions (performance+tracking) texts translations.

--- static/texts.js
+++ static/texts.js
@@ -1,4 +1,5 @@
 const TEXTS = {
+    /** en **/
     "Teamboarding - Revolutionize your Technical Onboarding Process": {
         "tracking": "Teamboarding - Enhance Team Impact through Performance Insights",
         "performance": "Teamboarding - Track, Optimize and Elevate Technical Performance"
@@ -66,5 +67,145 @@ const TEXTS = {
     "Adapts effortlessly to your existing tools and processes, eliminating costly and complex implementations.": {
         "tracking": "Adapts effortlessly to your existing tools and processes, eliminating costly and complex implementations.",
         "performance": "Adapts effortlessly to your existing tools and processes, eliminating costly and complex implementations."
+    },
+
+    /** es **/
+    "Teamboarding - Revoluciona tu proceso de incorporación técnica": {
+        "tracking": "Teamboarding - Mejora el impacto del equipo a través de conocimientos sobre performance",
+        "performance": "Teamboarding - Rastrea, optimiza y eleva la performance técnica"
+    },
+    "Revoluciona tu proceso de incorporación técnica": {
+        "tracking": "Mejora el impacto del equipo a través de conocimientos sobre performance",
+        "performance": "Rastrea, optimiza y eleva la performance técnica"
+    },
+    "Simplifica y mejora tu experiencia de incorporación, asegurando que cada nuevo miembro del equipo esté preparado para el éxito desde el primer día.": {
+        "tracking": "Facilita evaluaciones tanto de la experiencia técnica como de las habilidades blandas para mejorar la dinámica del equipo y fomentar una colaboración más efectiva.",
+        "performance": "Obtén conocimientos en tiempo real para impulsar la performance y la colaboración dentro de tu equipo técnico, empoderando la mejora continua."
+    },
+    "Acelera la integración técnica desde el primer día": {
+        "tracking": "Libera el potencial de tu equipo con conocimientos sobre performance",
+        "performance": "Libera todo el potencial de tu equipo con nuestras funcionalidades"
+    },
+    "Aprovecha funcionalidades avanzadas para lograr una integración técnica fluida y mejorar la efectividad desde el principio.": {
+        "tracking": "Maximiza la efectividad de tu equipo con capacidades que facilitan el seguimiento en tiempo real, información sobre performance y comunicación fluida.",
+        "performance": "Maximiza la efectividad de tu equipo con capacidades que facilitan el seguimiento en tiempo real, información sobre performance y comunicación fluida."
+    },
+    "Procesos Personalizados": {
+        "tracking": "Árbol de Conocimiento",
+        "performance": "Árbol de Conocimiento"
+    },
+    "Diseña y gestiona flujos de trabajo de incorporación adaptados a cada rol técnico.": {
+        "tracking": "Explora y gestiona las habilidades de los miembros de tu equipo, tanto técnicas como no técnicas, para una mejor colaboración.",
+        "performance": "Explora y gestiona las habilidades de los miembros de tu equipo, tanto técnicas como no técnicas, para una mejor colaboración."
+    },
+    "Métricas Detalladas": {
+        "tracking": "Métricas Detalladas",
+        "performance": "Métricas Detalladas"
+    },
+    "Evalúa el progreso de la incorporación con informes y análisis precisos.": {
+        "tracking": "Monitorea el progreso diario y la efectividad con informes completos y análisis precisos.",
+        "performance": "Monitorea el progreso diario y la efectividad con informes completos y análisis precisos."
+    },
+    "Incorporación organizada y simplificada": {
+        "tracking": "Seguimiento de rendimiento simplificado y fácil",
+        "performance": "Progreso diario simplificado y organizado"
+    },
+    "Descubre lo fácil que puede ser la integración técnica como nunca antes": {
+        "tracking": "Descubre lo sencillo que puede ser el seguimiento diario de performance como nunca antes.",
+        "performance": "Descubre lo sencilla que puede ser la integración diaria como nunca antes."
+    },
+    "Empareja fácilmente compañeros y líderes con miembros del equipo para una guía y apoyo efectivos.": {
+        "tracking": "Incorpora fácilmente a miembros experimentados para mejorar la guía y la colaboración dentro de tu equipo.",
+        "performance": "Incorpora fácilmente a miembros experimentados para mejorar la guía y la colaboración dentro de tu equipo."
+    },
+    "Conéctate con las herramientas habituales de los desarrolladores para recopilar métricas y rastrear el progreso del equipo.": {
+        "tracking": "Conéctate con las herramientas habituales de los desarrolladores para recopilar métricas y rastrear el progreso del equipo.",
+        "performance": "Conéctate con las herramientas habituales de los desarrolladores para recopilar métricas y rastrear el progreso del equipo."
+    },
+    "Rastrea y evalúa el proceso de incorporación de nuevos empleados, compañeros y líderes, todo desde una sola plataforma centralizada.": {
+        "tracking": "Rastrea y evalúa el rendimiento diario de tu equipo, todo desde una única plataforma centralizada.",
+        "performance": "Rastrea y evalúa el rendimiento diario de tu equipo, todo desde una única plataforma centralizada."
+    },
+    "Optimiza la incorporación y aumenta la eficiencia del equipo, integrándose sin problemas en tus procesos.": {
+        "tracking": "Optimiza el seguimiento diario y mejora la eficiencia del equipo, integrándose sin problemas en tus procesos.",
+        "performance": "Optimiza el seguimiento diario y mejora la eficiencia del equipo, integrándose sin problemas en tus procesos."
+    },
+    "Reduce gastos acelerando la incorporación, optimizando recursos y disminuyendo la rotación.": {
+        "tracking": "Reduce gastos optimizando recursos, mejorando el rendimiento diario y disminuyendo la rotación.",
+        "performance": "Reduce gastos optimizando recursos, mejorando el rendimiento diario y disminuyendo la rotación."
+    },
+    "Se adapta fácilmente a tus herramientas y procesos existentes, eliminando implementaciones costosas y complejas.": {
+        "tracking": "Se adapta fácilmente a tus herramientas y procesos existentes, eliminando implementaciones costosas y complejas.",
+        "performance": "Se adapta fácilmente a tus herramientas y procesos existentes, eliminando implementaciones costosas y complejas."
+    },
+
+    /** pt **/
+    "Teamboarding - Revolucione seu processo de integração técnica": {
+        "tracking": "Teamboarding - Melhore o impacto da equipe por meio de insights de desempenho",
+        "performance": "Teamboarding - Monitore, otimize e eleve o desempenho técnico"
+    },
+    "Revolucione seu processo de integração técnica": {
+        "tracking": "Melhore o impacto da equipe por meio de insights de desempenho",
+        "performance": "Monitore, otimize e eleve o desempenho técnico"
+    },
+    "Simplifique e melhore sua experiência de integração, garantindo que cada novo membro da equipe esteja preparado para o sucesso desde o primeiro dia.": {
+        "tracking": "Facilite avaliações de competências técnicas e habilidades interpessoais para melhorar a dinâmica da equipe e promover uma colaboração mais eficaz.",
+        "performance": "Obtenha insights em tempo real para impulsionar o desempenho e a colaboração em sua equipe técnica, promovendo melhorias contínuas."
+    },
+    "Acelere a integração técnica desde o primeiro dia": {
+        "tracking": "Desbloqueie o potencial da sua equipe com insights de desempenho",
+        "performance": "Desbloqueie todo o potencial da sua equipe com nossos recursos"
+    },
+    "Aproveite funcionalidades avançadas para alcançar uma integração técnica perfeita e aumentar a eficácia desde o início.": {
+        "tracking": "Maximize a eficácia da sua equipe com recursos que facilitam o monitoramento em tempo real, insights de desempenho e comunicação simplificada.",
+        "performance": "Maximize a eficácia da sua equipe com recursos que facilitam o monitoramento em tempo real, insights de desempenho e comunicação simplificada."
+    },
+    "Processos Personalizados": {
+        "tracking": "Árvore de Conhecimento",
+        "performance": "Árvore de Conhecimento"
+    },
+    "Projete e gerencie fluxos de trabalho de integração adaptados a cada função técnica.": {
+        "tracking": "Explore e gerencie as competências dos membros da sua equipe, tanto técnicas quanto interpessoais, para uma colaboração mais eficaz.",
+        "performance": "Explore e gerencie as competências dos membros da sua equipe, tanto técnicas quanto interpessoais, para uma colaboração mais eficaz."
+    },
+    "Métricas Detalhadas": {
+        "tracking": "Métricas Detalhadas",
+        "performance": "Métricas Detalhadas"
+    },
+    "Avalie o progresso da integração com relatórios e análises precisas.": {
+        "tracking": "Monitore o progresso diário e a eficácia com relatórios abrangentes e análises precisas.",
+        "performance": "Monitore o progresso diário e a eficácia com relatórios abrangentes e análises precisas."
+    },
+    "Integração simplificada e organizada": {
+        "tracking": "Monitoramento simplificado de desempenho facilitado",
+        "performance": "Progresso diário simplificado e organizado"
+    },
+    "Descubra como a integração técnica pode ser fácil como nunca antes": {
+        "tracking": "Descubra como o monitoramento diário de desempenho pode ser simples como nunca antes.",
+        "performance": "Descubra como a integração diária pode ser simples como nunca antes."
+    },
+    "Emparelhe facilmente líderes e colegas com membros da equipe para orientação e suporte eficazes.": {
+        "tracking": "Integre facilmente membros experientes para melhorar a orientação e a colaboração em sua equipe.",
+        "performance": "Integre facilmente membros experientes para melhorar a orientação e a colaboração em sua equipe."
+    },
+    "Conecte-se com as ferramentas habituais dos desenvolvedores para coletar métricas e monitorar o progresso da equipe.": {
+        "tracking": "Conecte-se com as ferramentas habituais dos desenvolvedores para coletar métricas e monitorar o progresso da equipe.",
+        "performance": "Conecte-se com as ferramentas habituais dos desenvolvedores para coletar métricas e monitorar o progresso da equipe."
+    },
+    "Acompanhe e avalie a jornada de integração de novos contratados, colegas e líderes, tudo a partir de uma única plataforma centralizada.": {
+        "tracking": "Acompanhe e avalie o desempenho diário da sua equipe, tudo a partir de uma única plataforma centralizada.",
+        "performance": "Acompanhe e avalie o desempenho diário da sua equipe, tudo a partir de uma única plataforma centralizada."
+    },
+    "Otimize a integração e aumente a eficiência da equipe, integrando-se perfeitamente aos seus processos.": {
+        "tracking": "Simplifique o monitoramento diário e aumente a eficiência da equipe, integrando-se perfeitamente aos seus processos.",
+        "performance": "Simplifique o monitoramento diário e aumente a eficiência da equipe, integrando-se perfeitamente aos seus processos."
+    },
+    "Reduza custos acelerando a integração, otimizando recursos e diminuindo a rotatividade.": {
+        "tracking": "Reduza custos otimizando recursos, melhorando o desempenho diário e diminuindo a rotatividade.",
+        "performance": "Reduza custos otimizando recursos, melhorando o desempenho diário e diminuindo a rotatividade."
+    },
+    "Adapta-se facilmente às suas ferramentas e processos existentes, eliminando implementações caras e complexas.": {
+        "tracking": "Adapta-se facilmente às suas ferramentas e processos existentes, eliminando implementações caras e complexas.",
+        "performance": "Adapta-se facilmente às suas ferramentas e processos existentes, eliminando implementações caras e complexas."
     }
 }
